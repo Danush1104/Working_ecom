53f9075a97dc76adf75d3761e39c8b7a6e2bf008
Danush1104 <danushnarayan11@gmail.com>
2026-08-04 12:41:52 +0530

    Checking CloudFront Frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { configureAmplify } from './aws-config'
 
-configureAmplify();
+configureAmplify();  
 
 createRoot(document.getElementById('root')!).render(
  <StrictMode>
